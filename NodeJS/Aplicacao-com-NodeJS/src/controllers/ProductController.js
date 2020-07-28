849e285a77158fba00360d178cc07fa7c7e96ef0
Dario-Felipe <dariofelipe91@gmail.com>
2020-07-28 16:24:14 -0300

    adding a plugin for pagination on mongoose for selects in BD. 'mongoose-paginate'.

--- NodeJS/Aplicacao-com-NodeJS/src/controllers/ProductController.js
+++ NodeJS/Aplicacao-com-NodeJS/src/controllers/ProductController.js
@@ -5,21 +5,26 @@ const Product = mongoose.model("Product");
 //Exportando funções a seram utilizadas
 module.exports = {
   async index(req, res) {
-    const products = await Product.find();    // Mostrar todos
+    const { page = 1 } = req.query;
+    const products = await Product.paginate( {}, { page, limit: 10 });    // Mostrar todos com paginação
     return res.json(products);
   },
+
   async store(req, res) {
     const product = await Product.create( req.body );   // Adicionar Elemento
     return res.json(product);
   },
+
   async show(req, res) {
     const product = await Product.findById( req.params.id );    // Mostrar por ID
     return res.json(product);
   },
+
   async update(req, res) {
     const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });  // Update por ID
     return res.json(product);
   },
+
   async delete(req, res) {
     await Product.findByIdAndRemove( req.params.id );   // Deletar por ID
     return res.send("Removido com Sucesso!");
