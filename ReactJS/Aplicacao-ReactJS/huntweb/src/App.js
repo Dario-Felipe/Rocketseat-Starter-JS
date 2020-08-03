0847ab574cc3c4c7497ddddf3cccc3397692241c
Dario-Felipe <dariofelipe91@gmail.com>
2020-08-03 16:23:25 -0300

    Now starting ReactJS concepts.

---
+++ ReactJS/Aplicacao-ReactJS/huntweb/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./styles.css";
+import Header from './components/Header'
+
+const App = () => (
+  <div className = "App">
+    <Header />
+  </div>
+);
+
+export default App;
