ff95bcc3b997ac9a548a92a031b0ccf63f71b44d
Dario-Felipe <dariofelipe91@gmail.com>
2020-08-28 13:53:50 -0300

    Adding a style for pages

--- ReactJS/Aplicacao-ReactJS/huntweb/src/pages/main/index.js
+++ ReactJS/Aplicacao-ReactJS/huntweb/src/pages/main/index.js
@@ -1,27 +1,61 @@
 import React, { Component } from "react";
-import api from "../../services/api"
+import api from "../../services/api";
+import "./style.css";
 
 export default class Main extends Component {
   state = {
     products: [],
-  }
+    productInfo: {},
+    page: 1,
+  };
 
   componentDidMount() {
     this.loadProducts();
   }
 
-  loadProducts = async () => {
-    const response = await api.get("/products");
-    this.setState({ products: response.data.docs });
+  loadProducts = async ( page = 1 ) => {
+    const response = await api.get(`/products?page=${ page }`);
+    const { docs, ...productInfo } = response.data;
+    this.setState({ products: docs, productInfo, page });
+  }
+
+  prevPage = () => {
+    const { page } = this.state;
+
+    if(page == 1) return;
+
+    const pageNumber = page - 1;
+
+    this.loadProducts(pageNumber);
+  }
+
+  nextPage = () => {
+    const { page, productInfo } = this.state;
+    
+    if( page == productInfo.pages ) return;
+    
+    const pageNumber = page + 1;
+
+    this.loadProducts(pageNumber);
   }
 
   render() {
+    const { products, productInfo, page } = this.state;
+
     return (
       <div className = "products-list"> 
-        { this.state.products.map( products => (
-          <h2 key = { products._id } > { products.title } </h2>
+        { products.map( products => (
+          <article key = { products._id }>
+            <strong> { products.title } </strong>
+            <p> {products.description } </p>
+            <a href = "#"> Acessar </a>
+          </article>
           )) 
         }
+        <div className = "actions">
+          <button disabled = { page == 1 } onClick = { this.prevPage }> Anterior </button>
+          <button disabled = { page == productInfo.pages } onClick = { this.nextPage }> Próxima </button>
+        </div>
       </div>
     );
   }
