ce6c9232874504a880d4f7907a84333447c0af97
Dario-Felipe <dariofelipe91@gmail.com>
2020-09-01 15:07:45 -0300

    Added a Datails page for all listed elements.

--- ReactJS/Aplicacao-ReactJS/huntweb/src/pages/product/index.js
+++ ReactJS/Aplicacao-ReactJS/huntweb/src/pages/product/index.js
@@ -2,18 +2,34 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 export default class Product extends Component {
+  state = {
+    product: {}
+  }
+
   componentDidMount() {
     this.loadProduct();
   }
-  
+
   loadProduct = async () => {
-    const response = await api.get('/products/:id');
-    console.log(response);
+    const { id } = this.props.match.params;
+
+    const response = await api.get(`/products/${id}`);
+
+    this.setState({ product: response.data });
   }
-  
+
   render() {
+    const { product } = this.state;
+
     return (
-      <h1> Product </h1>      
-    );
+      <div className="products-list">
+        <article key={product._id}>
+          <strong> {product.title} </strong>
+          <p><strong>Descrição:</strong> {product.description} </p>
+          <p><strong>Criado:</strong> { product.createdAt } </p>
+          <a href = { product.url } target = "_blank" > Acesse o site aqui! </a>
+        </article>
+      </div>
+    )
   }
 }
